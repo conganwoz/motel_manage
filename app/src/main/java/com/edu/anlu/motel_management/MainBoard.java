5fbb4f2b82c06d45070eba3f269bd992a900fb5c
anlu <conganwoz@gmail.com>
2019-04-29 08:26:17 +0700

    update fix import

--- app/src/main/java/com/edu/anlu/motel_management/MainBoard.java
+++ app/src/main/java/com/edu/anlu/motel_management/MainBoard.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TabHost;
@@ -211,4 +212,9 @@ public class MainBoard extends TabActivity implements NavigationView.OnNavigatio
 
             finish();
     }
+
+
+    private AdapterView.OnItemClickListener onListMotelClick = (parent, view, position, id) -> {
+
+    };
 }
